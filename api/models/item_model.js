c8c226e8bc307f8c8c1f5e1e5d2d7840dcbb427a
Vic <t18042k@gmail.com>
2020-11-18 03:41:44 +0200

    login, register basic page, backend edited databases for items

--- api/models/item_model.js
+++ api/models/item_model.js
@@ -3,8 +3,8 @@ const db = require("../database");
 const item = {
   add: function (item, callback) {
     return db.query(
-      "insert into item_table(id, user_id, name, photo_url, price) values($1,$2,$3, $4, $5) RETURNING *",
-      [item.id, item.user_id, item.name, item.photo_url, item.price],
+      "insert into item_table(id, user_id, name, photo_url, price, description, category) values($1,$2,$3, $4, $5, $6, $7) RETURNING *",
+      [item.id, item.user_id, item.name, item.photo_url, item.price, item.description, item.category],
       callback
     );
   },
