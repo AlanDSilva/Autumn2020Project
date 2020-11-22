cec33df3fe32c14e62718d7f3bb211ca02d25b12
Kuismai <32294061+Kuismai@users.noreply.github.com>
2020-11-22 19:47:28 +0200

    Adding product addition page

---
+++ client/src/components/AddProduct.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddProduct() {
+    return (
+        <div>
+            <br/><br/><br/><br/><br/><br/>Here you add products
+        </div>
+    )
+}
+
+export default AddProduct
