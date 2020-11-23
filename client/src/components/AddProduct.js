4ab34732af85ef76024dfe6394a88071fa832025
Kuismai <32294061+Kuismai@users.noreply.github.com>
2020-11-23 07:55:41 +0200

    Adding an item form

--- client/src/components/AddProduct.js
+++ client/src/components/AddProduct.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import constants from '../constants.json';
 
-function AddProduct() {
-    return (
-        <div>
-            <br/><br/><br/><br/><br/><br/>Here you add products
-        </div>
-    )
-}
-
-export default AddProduct
+export default function AddProduct(props) {
+    function add(event) {
+      event.preventDefault();
+      // get value from the form
+      var name = event.target['name'].value;
+      var picture = event.target['picture'].value;
+      var price = event.target['price'].value;
+      var description = event.target['description'].value;
+      var category = event.target['category'].value;
+  
+      axios.post(constants.baseAddress +'/api/items', {
+            name,
+            picture,
+            price,
+            description,
+            category
+      })
+      .then(function (response) {
+            props.history.push(props.redirectPathOnSuccess);
+      })
+      .catch(function (error) {
+            console.log(error);
+      });
+      } 
+      return (
+          <div> <br/><br/><br/><br/>
+              <form onSubmit={ add }>
+                  Item name: <input type="text" name="name"/><br />
+                  Picture URL: <input type="text" name="picture"/><br/>
+                  Price: <input type="text" name="price"/><br/>
+                  Item description: <input type="text" name="description"/><br/>
+                  Category: <input type="text" name="category"/><br/>
+                 <button type="submit" value="submit">Submit</button>
+              </form>
+          </div>
+        );
+  }
