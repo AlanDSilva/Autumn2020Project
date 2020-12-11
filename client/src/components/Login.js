5391fd886721e6e7ecebe5dea863a4f58751e332
Vic <t18042k@gmail.com>
2020-12-11 11:57:49 +0200

    logn, register init

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,63 +1,127 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from 'react';
+import { Redirect } from "react-router-dom";
+import {Link} from "react-router-dom";
+// style
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Checkbox from '@material-ui/core/Checkbox';
+// notification style
+import {toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-import constants from "../constants.json";
-import axios from "axios";
+import constants from '../constants.json';
+import axios from 'axios';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/collection/4511514)', //from https://unsplash.com/collections/4511514/clothing
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function Login(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
+  
+  const classes = useStyles();
+  
+  // noti
+  toast.configure();
+  function notify(value) {
+    value == 1 ? toast.success('Welcome back, customer :)') : toast.error("Wrong username or password :(")
+  }
   // login
-  const login = (event) => {
+  function login(event)
+  {
     event.preventDefault();
     // send to api
     axios
-      .post(constants.baseAddress + "/api/login", { username, password })
-      .then((res) => {
-        if (res.data) {
-          console.log(res.data);
-          localStorage.setItem("username", username);
-          localStorage.setItem("tokenUser", res.data.token);
-          localStorage.setItem("photo_url", res.data.photo_url); //store the username to the localstorage for futher use
-          props.history.push(props.redirectPathOnSuccess); // direct to main page if login success
-          // props.loginSuccess(); //set state to logged in
+      .post(constants.baseAddress+'/api/login', { username, password })
+      .then(res => {
+        if(res.data){
+            notify(1); // 1 mean success
+            localStorage.setItem("username", username);
+            localStorage.setItem("tokenUser", res.data.token);
+            localStorage.setItem("photo_url", res.data.photo_url); //store the email to the localstorage for futher use
+            props.history.push(props.redirectPathOnSuccess); // direct to main page if login success
+            props.loginSuccess(); //set state to logged in 
         }
       })
-      .catch((error) => {
+      .catch(error => {
         console.log(error.response);
+        notify(2); // fail
       });
-  };
-
-  return (
-    <div>
-      {" "}
-      <br />
-      <br />
-      <br />
-      <h3>Login form</h3>
-      <form onSubmit={login}>
-        Username:{" "}
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <br />
-        Password:{" "}
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <br />
-        <button type="submit">Login</button>
-        <Link to="/register">Register Here</Link>
-      </form>
-    </div>
-  );
+  }
+  if (props.isAuthenticated) { //if somebody tries to goes back to register
+    return(<React.Fragment><Redirect to='/' /></React.Fragment>)
+  } else {
+    return (
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}></Avatar> 
+            <Typography component="h1" variant="h5">Sign in</Typography>
+            
+            {/* form */}
+            <form className={classes.form} noValidate onSubmit={login}>
+              <TextField variant="outlined" margin="normal" fullWidth required label="Username"
+                                                                          type="text"
+                                                                          value={username}
+                                                                          onChange={(e) => {
+                                                                            setUsername(e.target.value);
+                                                                          }} />
+              <TextField variant="outlined" margin="normal" required fullWidth label="Password" type="password"  autoComplete="current-password"
+                                                                               name="password" value={password}
+                                                                               onChange={(e) => {
+                                                                                 setPassword(e.target.value);
+                                                                               }}/>  
+              <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me"  />
+              <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit} >
+                Sign In
+              </Button>
+              <Grid container>
+                <Grid item xs> </Grid>
+                <Grid item>
+                  <Link to="/register" variant="body2"> {"Don't have an account? Sign Up"} </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    );
+  }
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,16 +1,60 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import { Redirect, Link } from "react-router-dom";
-import axios from "axios";
-import constants from "../constants.json";
+// style
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+// notification style
+import {toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+import axios from 'axios';
+import constants from '../constants.json';
+
+// style
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 export default function Register(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
-
-  const register = (event) => {
+  // noti
+  toast.configure();
+  function notify(value) {
+    if (value == 1) toast.success('Create success ^_^') 
+    else if (value == 2) toast.error("Password must be more than 6 characters")
+    else if (value == 3) toast.warning("Invalid username")
+    else toast.warning("Missing input or invalid file")
+  }
+  // register
+  function register(event) {
     event.preventDefault();
-
+    
     // setup form data and configuration
     let formData = new FormData();
     formData.append("username", username);
@@ -21,58 +65,83 @@ export default function Register(props) {
         "content-type": "multipart/form-data",
       },
     };
-    axios
-      .post(constants.baseAddress + "/api/users", formData, config)
-      .then(function (res) {
-        console.log(res.data);
-        localStorage.setItem("username", res.data.username);
-        localStorage.setItem("tokenUser", res.data.token);
-        localStorage.setItem("photo_url", res.data.photo_url); //store the username to the localstorage for futher use
-        props.history.push(props.redirectPathOnSuccess); // direct to main page if login success
-        // props.loginSuccess(); //set state to logged in
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
+    // check if email is valid
+    if (username.length < 4 ) {
+        notify(3);
+    }      
+    // then check that password is more than 6
+    else if(password.length < 6 )  notify(2); //warning noti
+    else {
+        // everything works well
+        axios
+          .post(constants.baseAddress + "/api/users", formData, config)
+          .then(function (res) {
+            console.log(res.data);
+            localStorage.setItem("username", res.data.username);
+            localStorage.setItem("tokenUser", res.data.token);
+            localStorage.setItem("photo_url", res.data.photo_url); //store the username to the localstorage for futher use
+            props.history.push(props.redirectPathOnSuccess); // direct to main page if login success
+            notify(1);
+          })
+          .catch(function (error) {
+            notify(4)
+            console.log(error);
+          });
+      }
+    }
+  
+
+  const classes = useStyles();
+  // if someone try to go to register but still logged in, goes back to main page
+  if (props.isAuthenticated) {
+    return(<React.Fragment><Redirect to='/' /></React.Fragment>)
+  } else {
   return (
-    <div>
-      {" "}
-      <br />
-      <br />
-      <br />
-      <br />
-      <h3>Register form</h3>
-      <form onSubmit={register} encType="multipart/form-data">
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />{" "}
-        <br />
-        Photo:
-        <input
-          type="file"
-          //value={selectedFile}
-          onChange={(e) => {
-            setSelectedFile(e.target.files[0]);
-          }}
-        />{" "}
-        <br />
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />{" "}
-        <br />
-        <button type="submit">Register</button>
-      </form>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={ register }>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField  variant="outlined" required fullWidth id="email" label="Email Address" autoComplete="email"
+                                                                                   value={username}
+                                                                                   onChange={(e) => {
+                                                                                     setUsername(e.target.value);
+                                                                                   }}/>
+            </Grid>
+
+            <Grid item xs={12}>
+              <TextField variant="outlined" required fullWidth  id="lastName" 
+                                                                                    type="file"
+                                                                                    onChange={(e) => {
+                                                                                      setSelectedFile(e.target.files[0]);
+                                                                                    }}  />
+            </Grid>
+            
+            <Grid item xs={12}>
+              <TextField variant="outlined" required fullWidth label="Password" type="password" autoComplete="current-password"
+                                                                                   value={password}
+                                                                                   onChange={(e) => {
+                                                                                     setPassword(e.target.value);
+                                                                                   }}  />
+            </Grid>
+          </Grid>
+          <Button  type="submit"  fullWidth variant="contained" color="primary" className={classes.submit} >
+                Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+                <Link to="/login" variant="body2"> {"Already have an account? Sign in"} </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
-}
+}}
