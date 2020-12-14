ba6e6be53582a391a1f1fa2ca4e64affac994635
Kuismai <32294061+Kuismai@users.noreply.github.com>
2020-12-14 13:13:50 +0200

    Added an FAQ

--- client/src/components/mainpages/Help.js
+++ client/src/components/mainpages/Help.js
@@ -3,7 +3,24 @@ import React from 'react'
 function Help() {
     return (
         <div>
-            <br/><br/><br/><br/>Here is Donation page
+            <br/><br/><br/><br/><b><u>FAQ</u></b>
+
+            <p><b>Who are you?</b>
+            Our company is dedicated to helping you buy and sell your second-hand items in a sustainable manner. 
+            </p>
+
+            <p><b>Why should I use your service?</b>
+            Besides helping sellers and buyers find each other, we also offer a layer of protection for your payments.
+            </p>
+
+            <p><b>How is your company funded?</b>
+            We take a fee from the transaction payments made on the site. The seller doesn't have to pay anything; the fee is added on the buyer side of things. If your items don't sell, you don't pay anything, nor do you have to pay for simply browsing the selection of items.
+            </p>
+
+
+            <p><b>Is any of this real?</b>
+            Ahaha, nope. This is all for course credit. Please do not try to buy or sell actual items here even if you somehow found your way to the site (how did you do that, anyway?).
+            </p>
         </div>
     )
 }
